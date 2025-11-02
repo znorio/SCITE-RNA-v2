b889567925db4dcda5677f0f4d6d379034af55b8
Norio <znorio@ethz.ch>
2025-11-02 12:05:42 +0100

    Updating for Review 1

--- generate_results_cpp/comparison_parameters.cpp
+++ generate_results_cpp/comparison_parameters.cpp
@@ -27,6 +27,22 @@ int main() {
     load_config("../config/config.yaml");
     std::cout << "Random seed: " << config_variables["random_seed"] << std::endl;
 
+    std::vector<std::string> param_names = {
+            "dropout", "overdispersion_Het", "overdispersion_Hom", "error_rate",
+            "coverage_mean", "coverage_zero_inflation", "coverage_dispersion"
+    };
+
+    std::vector<std::vector<std::string>> param_values = {
+            {"0", "0_2", "0_4", "0_6"},
+            {"3", "6", "10", "100"},
+            {"3", "6", "10", "100"},
+            {"0_001", "0_01", "0_05", "0_1"},
+            {"10", "30", "60", "100"},
+            {"0", "0_2", "0_4", "0_6"},
+            {"1", "2", "5", "10"}
+    };
+
+
     for (const auto& clone : clones) {
         for (int i = 0; i < n_cells_list.size(); ++i) {
             int n_cells = n_cells_list[i];
@@ -36,9 +52,21 @@ int main() {
                 insert_nodes = false; // disable node insertion for large datasets to speed up inference
             }
 
-            std::string path = "../data/simulated_data/" + std::to_string(n_cells) + "c" + std::to_string(n_mut) + "m" + clone +"/";
-            std::string pathout = path +  "sciterna/";
-            generate_sciterna_simulation_results(path, pathout, n_tests, tree_space, flipped_mutation_direction, n_mut, n_rounds, insert_nodes=insert_nodes);
+            for (size_t p = 0; p < param_names.size(); ++p) {
+                for (const auto& val : param_values[p]) {
+                    std::string param_str = param_names[p] + "_" + val;
+                    std::string base_path = "../data/simulated_data/" + std::to_string(n_cells) + "c" +
+                                            std::to_string(n_mut) + "m" + clone + "_param_testing/";
+                    std::string path = base_path + param_str + "/";
+                    std::string pathout = path + "sciterna/";
+
+                    generate_sciterna_simulation_results(
+                            path, pathout, n_tests, tree_space,
+                            flipped_mutation_direction, n_mut, n_rounds,
+                            insert_nodes
+                    );
+                }
+            }
         }
     }
     return 0;
